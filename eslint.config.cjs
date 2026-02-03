bb39d9e5aa1713cfc533a65987083d3644d8f91e
bakosi-exesoft <bakosi.peter@exesoft.hu>
2026-02-03 13:56:47 +0100

    Express and dev tools installed

---
+++ eslint.config.cjs
@@ -0,0 +1,58 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+const nodeAndES2021Globals = {
+  require: 'readonly',
+  module: 'readonly',
+  __dirname: 'readonly',
+  __filename: 'readonly',
+  exports: 'readonly',
+  process: 'readonly',
+  Buffer: 'readonly',
+  setImmediate: 'readonly',
+  clearImmediate: 'readonly',
+  global: 'readonly',
+  Promise: 'readonly',
+  Symbol: 'readonly',
+  Map: 'readonly',
+  Set: 'readonly',
+  WeakMap: 'readonly',
+  WeakSet: 'readonly',
+};
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: nodeAndES2021Globals,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      globals: nodeAndES2021Globals,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+];
