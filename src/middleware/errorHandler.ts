647f54ba11d5ce0a2cdf808726f248c7547910e8
bakosi-exesoft <bakosi.peter@exesoft.hu>
2026-02-04 10:50:20 +0100

    Nodemon initialized

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export const errorHandler = (err: any, req: Request, res: Response) => {
+// eslint-disable-next-line (no-unused-vars)
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status || 500).json({
     message: err.message || 'Internal server error'
   });
