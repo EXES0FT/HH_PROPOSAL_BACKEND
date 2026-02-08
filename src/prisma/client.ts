d79cc847e36476a340f871df1d26dba335f8a5e9
bakosi-exesoft <bakosi.peter@exesoft.hu>
2026-02-08 12:46:25 +0100

    Prisma updated to latest

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from './generated/prisma-client';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: 'localhost',
+  port: 3306,
+  connectionLimit: 5,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
